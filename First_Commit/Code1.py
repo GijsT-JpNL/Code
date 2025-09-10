45e114752d0032d0d0a8f378a35c7df4d81da8c6
Gijs <99085303@mydavinci.nl>
2025-09-10 15:33:24 +0200

    1

--- First_Commit/Code1.py
+++ First_Commit/Code1.py
@@ -1,2 +1,13 @@
 import random
-print(random.randrange(1, 100))
+
+while True:
+    command = input("Typ 'generate' om een getal te genereren of 'stop' om te stoppen: ")
+    
+    if command.lower() == "generate":
+        number = random.randrange(1, 100)
+        print(f"🎲 Je willekeurige getal is: {number}")
+    elif command.lower() == "stop":
+        print("Programma gestopt. Tot de volgende keer!")
+        break
+    else:
+        print("Ongeldig commando. Typ 'generate' of 'stop'.")
---
+++ bc_l2_doemee.py
@@ -0,0 +1,4 @@
+#Krijgt dit dezelfde uitkomst? 
+print( 3.40 / 100 * 9 )
+print( 2.45 / 100 * 9 )
+print( 1.95 / 100 * 9 )
