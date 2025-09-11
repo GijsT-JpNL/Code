b9e5df2f94cf061576be0d4f36160629d3fa0dec
Gijs <99085303@mydavinci.nl>
2025-09-11 13:47:53 +0200

    e

---
+++ bc_l3_opdracht1.py
@@ -0,0 +1,10 @@
+description = 'jaar'
+
+year = 1
+month = year * 12
+days = month * 30
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print(f'In een {description} zitten {seconds} seconden')
