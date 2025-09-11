fea892faf4ee732a7dd3e3fa3deeb8b1b426c38b
Gijs <99085303@mydavinci.nl>
2025-09-11 13:38:17 +0200

    5

---
+++ bc_l3_doemee.py
@@ -0,0 +1,12 @@
+# Wat gaat er gebeuren?
+getal1 = 25
+getal2 = 0
+print(0/25)
+naam = input('wat is jouw naam?')
+
+# we gaan hier een variable inbouwen
+print('Hallo ' + naam + ', ik leer nu programmeren.')
+
+woord = input('Geef mij een woord, en ik vertel hoeveel tekens erin zitten!')
+aantal_tekens = len(woord)
+print(f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.")
